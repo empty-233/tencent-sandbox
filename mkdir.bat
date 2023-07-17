1ecf0099bb811bf60bb7e8768024636d6f85aee0
“BrackRat” <1217821075@qq.com>
2023-07-17 15:23:40 +0800

    增加一键创建目录bat

---
+++ mkdir.bat
@@ -0,0 +1,13 @@
+@echo off
+md "App\QQ"
+md "App\QQNT"
+md "App\TIM"
+md "App\WeChat"
+md "Data\Common Files"
+md "Data\Documents\Tencent"
+md "Data\Documents\WeChat"
+md "Data\Roaming\Tencent"
+md "Data\Roaming\WeChat"
+md "Data\SysWOW64"
+md "Desktop"
+md "Scripts"
