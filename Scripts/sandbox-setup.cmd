4e00e106861707bf3f7f67c21896b9cef51862aa
Debug-CMU <39654172+Debug-CMU@users.noreply.github.com>
2023-07-25 18:46:53 +0000

    Fix addzh-cn.ps "not digitally signed" issue by bypassing the execution policy check

--- Scripts/sandbox-setup.cmd
+++ Scripts/sandbox-setup.cmd
@@ -1,5 +1,3 @@
-powershell.exe -command "&{Set-ExecutionPolicy RemoteSigned -force}"
-
-powershell.exe -file C:\scripts\addzh-cn.ps1
+powershell.exe -executionpolicy bypass -file C:\Scripts\addzh-cn.ps1
 
 C:\Program Files (x86)\Common Files\Tencent\QQProtect\Bin\QQProtect.exe
