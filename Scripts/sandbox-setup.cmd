cb4efe46750681f17225bcbefe7a0b40e3bab8ec
flower_elf <47104993+flower-elf@users.noreply.github.com>
2023-07-25 21:57:36 +0800

    Simplify

--- Scripts/sandbox-setup.cmd
+++ Scripts/sandbox-setup.cmd
@@ -1,3 +1,5 @@
 powershell.exe -command "&{Set-ExecutionPolicy RemoteSigned -force}"
 
 powershell.exe -file C:\scripts\addzh-cn.ps1
+
+C:\Program Files (x86)\Common Files\Tencent\QQProtect\Bin\QQProtect.exe
